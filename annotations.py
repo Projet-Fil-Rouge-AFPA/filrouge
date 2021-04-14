64cacbce792995ab08991aca333cdd2ba3b0e6fc
Vincent Pauwels <vincentpauwels8@gmail.com>
2021-04-14 13:01:14 +0200

    Added aggregator operation for get_annotations_vid

--- annotations.py
+++ annotations.py
@@ -28,7 +28,7 @@ def convert_start_time(start_time_string):
     minutes, seconds = map(int,start_time_string.split('.'))
     return 60 * minutes + seconds
 
-def get_annotations_video(filename_annotations, video_name):
+def get_annotations_video(filename_annotations, video_name, operator='first'):
     """ Get the annotations from the Google sheet file
         - response 8 start time
         - stress scores
@@ -36,30 +36,51 @@ def get_annotations_video(filename_annotations, video_name):
     Args:
         filename_annotations (string): path of the video annotation file
         video_name (string): name of the video (e.g "Video_1")
+        operator(string): first, mean, max, min, sum | default = first
 
     Returns:
         response8_start_time (int): start time of response 8 ('' if the video name is not found)
         stress_annotations ([int]): stress scores ('' if the video name is not found)
     """
     df_annotations = pd.read_csv(filename_annotations,  header=None).drop([0, 1, 2, 3])
+    df_annotations_video = df_annotations[df_annotations.iloc[:,1] == video_name]
 
     try:
-        # we take the first line of the annotations
-        # this can be changed
-        response8_start_time = df_annotations[df_annotations.iloc[:,1] == video_name].iloc[0,3]
-        stress_annotations = df_annotations[df_annotations.iloc[:,1] == video_name].iloc[0,4:18]
+
+        # Get Q8 start time
+        response8_start_time = df_annotations_video.iloc[0,3]
+        response8_start_time = convert_start_time(response8_start_time)
+
+        # Load annotations as array of float
+        stress_annotations_1 = np.array(df_annotations_video.iloc[0,4:18].astype(float))
+        stress_annotations_2 = np.array(df_annotations_video.iloc[1,4:18].astype(float))
+
+        # Aggregate annotation
+        stress_annotations = stress_annotations_1
+        if operator == 'first':
+            stress_annotations = stress_annotations_1
+        elif operator == 'mean':
+            stress_annotations = (stress_annotations_1 + stress_annotations_2) / 2
+        elif operator == 'sum':
+            stress_annotations = stress_annotations_1 + stress_annotations_2
+        elif operator == 'max':
+            stress_annotations = np.maximum(stress_annotations_1,stress_annotations_2)
+        elif operator == 'min':
+            stress_annotations = np.minimum(stress_annotations_1,stress_annotations_2)
+
+    #Error handling
     except IndexError as ex:
         print("Video name is not found in the annotation file", video_name, filename_annotations)
         return '', '' # the video_name was not found
 
-    if (pd.isna(response8_start_time)) or (stress_annotations.isna().any()) :
+    if (pd.isna(response8_start_time)) or (stress_annotations_1.isna().any()) :
         print("The video was not found in the annotation file or is not fully annotated ", 
                 filename_annotations, video_name)
         return '', '' # the video_name was not found
 
-    response8_start_time = convert_start_time(response8_start_time)
-    stress_annotations = [int(stress) for stress in stress_annotations]
-    
+    #Convert stress_annotations from np.array to list
+    stress_annotations = stress_annotations.tolist()
+
     return response8_start_time, stress_annotations
 
 def add_video_annotations(df_features, filename_annotations, time_column_index, video_name):
@@ -94,15 +115,14 @@ def add_video_annotations(df_features, filename_annotations, time_column_index, 
     for limit_2 in sequences_boundaries:
         # print(limit_1, limit_2)
         # df_list.append(df[(df['frameTime'] >= limit_1) & (df['frameTime'] < limit_2)])
-        df.loc[(df.iloc[:,time_column_index] >= limit_1) & (df.iloc[:,time_column_index] < limit_2),['stress','diapo']] =[stress_annotations[i],diapos[i]]
+        df.loc[(df.iloc[:,time_column_index] >= limit_1) & (df.iloc[:,time_column_index] < limit_2),['stress','diapo']] = [stress_annotations[i],diapos[i]]
         limit_1 = limit_2
         i += 1
     
     # last sequence
-    df.loc[(df.iloc[:,time_column_index] >= limit_2),['stress','diapo']] =[stress_annotations[i],diapos[i]]
+    df.loc[(df.iloc[:,time_column_index] >= limit_2),['stress','diapo']] =  [stress_annotations[i],diapos[i]]
     df.diapo = df.diapo.astype(int)
     df.stress = df.stress.astype(int)
     df.stress_global = df.stress_global.astype(int)
 
     return df
-
