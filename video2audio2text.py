c8fd19ba6bbc803dc17df357a48f752051c8fe97
Alexandre Mondin <Alex@MacBook-Air-Alex.local>
2021-04-21 14:47:42 +0200

    Update for automatic transcription

--- video2audio2text.py
+++ video2audio2text.py
@@ -4,6 +4,7 @@
 import os
 import moviepy.editor as mp
 import speech_recognition as sr
+import pandas as pd
 
 def load_write_audio(rep,video_dir,file):
     
@@ -12,7 +13,7 @@ def load_write_audio(rep,video_dir,file):
     
     return
     
-def load_write_transcript(rep,audio_dir,file,language,start,duration):
+def load_write_transcript(rep,audio_dir,file,file_text,language,start,duration):
     
     r = sr.Recognizer()
     audio_file = sr.AudioFile(f'{rep}/{audio_dir}/{file}')
@@ -20,7 +21,7 @@ def load_write_transcript(rep,audio_dir,file,language,start,duration):
         audio = r.record(source,offset=start,duration=duration)
         transcript = r.recognize_google(audio,language=language)    
     
-    transcript_file = file[:-4]+'.txt' #Replace '.wav' extension with '.txt
+    transcript_file = file_text #Replace '.wav' extension with '.txt
     
     with open(transcript_file,'w') as file:
                     file.write(transcript)
@@ -36,27 +37,35 @@ if __name__ == '__main__':
     text_dir = 'Texts_test'
     
     language = 'fr-FR'
-    start = 160
-    duration = 60
     
-    video_files = os.listdir(f'{rep}/{video_dir}/')
+    video_time_df = pd.read_csv('Questions_Time.csv',header=None)
+    
+    video_files = video_time_df[0].to_list()#os.listdir(f'{rep}/{video_dir}/')
     
     os.chdir(f'{rep}/{audio_dir}/')
     
     for file in video_files:
         
-        if file[-3:]=='mp4':
-            load_write_audio(rep,video_dir,file)
-            
+        file_extension = f'{file}.mp4'
+        
+        #if file[-3:]=='mp4':
+        load_write_audio(rep,video_dir,file_extension)
     
-    audio_files = os.listdir(f'{rep}/{audio_dir}/')
+            
+    audio_files = video_files#os.listdir(f'{rep}/{audio_dir}/')
     
     os.chdir(f'{rep}/{text_dir}/')
     
-    for file in audio_files:
+    for (i,file) in enumerate(audio_files):
         
-        if file[-3:]=='wav':
-            load_write_transcript(rep,audio_dir,file,language,start-1,duration+1)
+        file_extension = f'{file}.wav'
+        
     
-
-    
+        #if file[-3:]=='wav':
+        for j in range(1,9,2):
+            
+            start = video_time_df.loc[i,j]
+            duration = video_time_df.loc[i,j+1] - start
+            file_text = f'{file}_{j//2+1}.txt'
+            
+            load_write_transcript(rep,audio_dir,file_extension,file_text,language,start-0.5,duration+0.5)
