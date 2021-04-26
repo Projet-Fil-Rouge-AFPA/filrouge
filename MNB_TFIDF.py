11f87cbb97b94dab38e6c1d72c217ff009594ece
Alexandre Mondin <Alex@MacBook-Air-Alex.local>
2021-04-26 23:38:25 +0200

    New models

--- MNB_TFIDF.py
+++ MNB_TFIDF.py
@@ -5,29 +5,41 @@ import os
 import pandas as pd
 import numpy as np
 import spacy
+from annotations import get_annotations_video
 from sklearn.feature_extraction.text import CountVectorizer,TfidfTransformer
 from sklearn.naive_bayes import MultinomialNB
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.svm import SVC
 from sklearn.model_selection import StratifiedKFold
 
 folder_Code = '/Users/Alex/Cours_Telecom/INFMDI 780/Code/'
 folder_Data = '/Users/Alex/Cours_Telecom/INFMDI 780/Data/'
 folder_transcript = f'{folder_Data}/Texts_test_man/'
 
-file_labels = 'Annotations_man.csv'
+filename_annotations ='https://docs.google.com/spreadsheets/d/1Rqu1sJiD-ogc4a6R491JTiaYacptOTqh6DKqhwTa8NA/gviz/tq?tqx=out:csv&sheet=Template'
 file_sw = 'french_stop_words.txt'
 
 
 #Retrieve labels
-filename = f'{folder_Code}{file_labels}'
+df_annotations = pd.read_csv(filename_annotations,  header=None).drop([0, 1, 2, 3])
 
-labels = pd.read_csv(filename, header=None)
+video_names = set(df_annotations[1].values)
 
-files = labels[0].to_list()
+dict = {}
 
-text_files = []
+for video in video_names:
 
-for file in files:    
-    text_files.append(file)
+    for i in range(1,5):
+        text_file = f'{video}_{i}.txt'
+        label = get_annotations_video(filename_annotations, video,'max')[2]
+        gender = get_annotations_video(filename_annotations, video,'max')[4]    
+        gender_bool = 1.0 if gender == 'H' else 0.0
+
+        dict[text_file] = (label[i-1],gender_bool)
+
+df_labels = pd.DataFrame.from_dict(dict,columns=['Label','Gender'],orient='index')
+
+text_files = df_labels.index
 
 #Retrieve transcripts
 transcripts = []
@@ -80,22 +92,39 @@ X_tfidf = tfidf.fit_transform(X_vect)
 
 #Multinomial Naive Bayes
 clf = MultinomialNB()
+clf_RF = RandomForestClassifier()
+clf_SVC = SVC()
 
 #Stratified cross-validation
 skf = StratifiedKFold(n_splits=5,random_state=42, shuffle=True)
 
 X_cv = X_tfidf.toarray()
 
-y_cv = labels[1].values
+#np_gender = df_labels['Gender'].to_numpy().reshape(-1,1)
+#X_cv_gender = np.concatenate((X_cv,np_gender),axis=1)
+
+y_cv = df_labels['Label'].values
 
 scores_list = []
+scores_list_RF = []
+scores_list_SVC = []
 
 for train_index, test_index in skf.split(X_cv, y_cv):
     X_train, X_test = X_cv[train_index], X_cv[test_index]
     y_train, y_test = y_cv[train_index], y_cv[test_index]
     
     clf.fit(X_train,y_train)
+    clf_RF.fit(X_train,y_train)
+    clf_SVC.fit(X_train,y_train)
+    
     score = clf.score(X_test,y_test)
+    score_RF = clf_RF.score(X_test,y_test)
+    score_SVC = clf_SVC.score(X_test,y_test)
+    
     scores_list.append(score)
+    scores_list_RF.append(score_RF)
+    scores_list_SVC.append(score_SVC)
 
-print(np.mean(scores_list))
+print(f'MNB : {np.mean(scores_list)}')
+print(f'RF : {np.mean(scores_list_RF)}')
+print(f'SVC : {np.mean(scores_list_SVC)}')
