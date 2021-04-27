b8f5645fc06b6c516617366c9212b636a349b06d
Vincent Pauwels <vincentpauwels8@gmail.com>
2021-04-27 15:46:38 +0200

    added feat importance

---
+++ util.py
@@ -0,0 +1,29 @@
+import matplotlib.pyplot as plt
+import numpy as np
+import seaborn as sns
+def plot_feature_importance(importance,names,model_type):
+    """
+    Plot feature importance
+
+    Parameters
+    ----------
+    importance: model.feature_importances_
+    names: x.columns
+    model_type: 'Random Forest'
+    """
+    #Create arrays from feature importance and feature names
+    feature_importance = np.array(importance)
+    feature_names = np.array(names)
+    #Create a DataFrame using a Dictionary
+    data={'feature_names':feature_names,'feature_importance':feature_importance}
+    fi_df = pd.DataFrame(data)
+    #Sort the DataFrame in order decreasing feature importance
+    fi_df.sort_values(by=['feature_importance'], ascending=False,inplace=True)
+    #Define size of bar plot
+    plt.figure(figsize=(10,8))
+    #Plot Searborn bar chart
+    sns.barplot(x=fi_df['feature_importance'], y=fi_df['feature_names'])
+    #Add chart labels
+    plt.title(model_type + ' Feature Importance')
+    plt.xlabel('FEATURE IMPORTANCE')
+    plt.ylabel('FEATURE NAMES')
