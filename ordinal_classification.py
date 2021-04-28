312c53f61ac291306cd0cceb29bb0f11d2b3510f
Vincent Pauwels <vincentpauwels8@gmail.com>
2021-04-28 13:59:56 +0200

    added crossval, f1score

--- ordinal_classification.py
+++ ordinal_classification.py
@@ -17,15 +17,20 @@ def multi_to_binary(y):
     Returns:
         y_1: binary targets 0 (0) vs 1,2 (1)
         y_2: binary targets 0,1 (0) vs 2
+        y_3: binary targets 0,1,2 (0) vs 3 (1)
     """
     y = y.squeeze().astype(int)
     #Convert to binary targets
     y_1 = y.replace(2, 1)
-    
+
     y_2 = y.replace(1, 0)
     y_2 = y_2.replace(2, 1)
 
-    targets = [y_1, y_2]
+    y_3 = y.replace(1, 0)
+    y_3 = y_3.replace(2, 0)
+    y_3 = y_3.replace(3, 1)
+
+    targets = [y_1, y_2, y_3]
     return targets
 
 def predict_classifiers(list_models, x_test):
@@ -47,7 +52,8 @@ def predict_classifiers(list_models, x_test):
     preds = {}
     preds[0] = 1 - list_pred[0]
     preds[1] = list_pred[0] - list_pred[1]
-    preds[2] = list_pred[1]
+    preds[2] = list_pred[1] - list_pred[2]
+    preds[3] = list_pred[2]
 
     #Get argmax of dataframe -> get the index of the highest probability which corresponds to the label
     preds = pd.DataFrame.from_dict(preds,orient='index').transpose()
@@ -68,5 +74,6 @@ def ordinal_classification(list_models, x_train, y_train, x_test):
     y_train_targets = multi_to_binary(y_train)
     list_models[0].fit(x_train, y_train_targets[0])
     list_models[1].fit(x_train, y_train_targets[1])
+    list_models[2].fit(x_train, y_train_targets[2])
     preds_argmax = predict_classifiers(list_models,x_test)
     return preds_argmax
