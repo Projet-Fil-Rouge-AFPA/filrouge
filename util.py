14e8235d18a5f5e3cc1cf831d9d78cea60a4276e
Vincent Pauwels <vincentpauwels8@gmail.com>
2021-04-29 19:58:53 +0200

    update notebook

--- util.py
+++ util.py
@@ -1,6 +1,8 @@
 import matplotlib.pyplot as plt
 import numpy as np
+import pandas as pd
 import seaborn as sns
+
 def plot_feature_importance(importance,names,model_type):
     """
     Plot feature importance
